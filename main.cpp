8d49073d3d6cc1042343bde75d1e565ec5ee2094
mallikarjunSN <mallikarjunsn3@gmail.com>
2019-11-11 18:32:26 +0530

    new commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-using namespace std;
-
-void swap(int *a,int *b){
-	*a= *a + *b;
-	*a= *a - *b;
-	*b= *a - *b;
-}
-
-int main(){
-	int a=4,b=5;
-	printf("\nhello man");
-	swap(&a,&b);
-	return 0;
-}
-
+//this is a little modification
